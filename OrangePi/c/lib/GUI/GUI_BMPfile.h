5cfa187b193975bb82215d369b727963a5c24b52
Partiolainen <petersson.kiev@gmail.com>
2023-08-01 13:53:38 +0300

    updated GUI

--- OrangePi/c/lib/GUI/GUI_BMPfile.h
+++ OrangePi/c/lib/GUI/GUI_BMPfile.h
@@ -6,10 +6,12 @@
 *                Used to shield the underlying layers of each master
 *                and enhance portability
 *----------------
-* |	This version:   V2.2
-* | Date        :   2020-07-08
+* |	This version:   V2.3
+* | Date        :   2020-07-27
 * | Info        :   
 * -----------------------------------------------------------------------------
+* V2.3(2022-07-27):
+* 1.Add GUI_ReadBmp_RGB_4Color()
 * V2.2(2020-07-08):
 * 1.Add GUI_ReadBmp_RGB_7Color()
 * V2.1(2019-10-10):
