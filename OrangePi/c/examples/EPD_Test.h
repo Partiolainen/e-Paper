af8dbf017844354b54d95b44efae0a875687daf9
Partiolainen <petersson.kiev@gmail.com>
2023-08-01 13:14:02 +0300

    init

--- OrangePi/c/examples/EPD_Test.h
+++ OrangePi/c/examples/EPD_Test.h
@@ -1,11 +1,11 @@
 /*****************************************************************************
-* | File      	:	  EPD_Test.h
+* | File      	:	EPD_Test.h
 * | Author      :   Waveshare team
 * | Function    :   e-Paper test Demo
 * | Info        :
 *----------------
-* |	This version:   V1.0
-* | Date        :   2019-06-11
+* |	This version:   V1.1
+* | Date        :   2022-07-28
 * | Info        :   
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -37,6 +37,15 @@
 #include "Debug.h"
 #include <stdlib.h> // malloc() free()
 
+int EPD_1in64g_test(void);
+int EPD_2in36g_test(void);
+int EPD_3in0g_test(void);
+int EPD_4in37g_test(void);
+int EPD_7in3g_test(void);
+
+int EPD_1in54_DES_test(void);
+int EPD_2in13_DES_test(void);
+int EPD_2in9_DES_test(void);
 
 int EPD_1in02d_test(void);
 
@@ -49,6 +58,7 @@ int EPD_1in54c_test(void);
 int EPD_2in66_test(void);
 int	EPD_2in66b_test(void); 
 int EPD_2in7_test(void);
+int EPD_2in7_V2_test(void);
 int EPD_2in7b_test(void);
 int EPD_2in7b_V2_test(void);
 
@@ -60,17 +70,25 @@ int EPD_2in9d_test(void);
 
 int EPD_2in13_test(void);
 int EPD_2in13_V2_test(void);
+int EPD_2in13_V3_test(void);
 int EPD_2in13bc_test(void);
 int EPD_2in13b_V3_test(void);
+int EPD_2in13b_V4_test(void);
 int EPD_2in13d_test(void);
+int EPD_2in13g_test(void);
+
+int EPD_3in52_test(void);
 
 int EPD_3in7_test(void);
 
 int EPD_4in01f_test(void);
 
 int EPD_4in2_test(void);
+int EPD_4in2_V2_test(void);
 int EPD_4in2bc_test(void);
 int EPD_4in2b_V2_test(void);
+
+int EPD_4in37b_test(void);
 
 int EPD_5in65f_test(void);
 
@@ -79,10 +97,18 @@ int EPD_5in83_V2_test(void);
 int EPD_5in83bc_test(void);
 int EPD_5in83b_V2_test(void);
 
+int EPD_5in84_test(void);
+
+int EPD_7in3f_test(void);
+
 int EPD_7in5_test(void);
 int EPD_7in5_HD_test(void);
 int EPD_7in5_V2_test(void);
 int EPD_7in5bc_test(void);
 int EPD_7in5b_V2_test(void);
 int EPD_7in5b_HD_test(void);
+
+int EPD_10in2b_test(void);
+
+int EPD_13in3_test(void);
 #endif
